9920632efbe04a5391421a1f17509954d5493d07
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-09-05 08:54:51 -0400

    completed all user stories

--- src/com/skilldistillery/jets/entities/ElectronicWarfarePlane.java
+++ src/com/skilldistillery/jets/entities/ElectronicWarfarePlane.java
@@ -5,9 +5,5 @@ public class ElectronicWarfarePlane extends Airplane{
 	public ElectronicWarfarePlane(String model, double speed, int range, long price) {
 		super(model, speed, range, price);
 	}
-	
-	public void conductEWMission() {
 		
-	}
-	
 }
